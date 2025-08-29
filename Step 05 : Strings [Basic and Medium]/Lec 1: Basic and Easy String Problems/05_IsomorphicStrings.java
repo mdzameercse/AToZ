8e52da8beb8af31095dcc5761a3df1c01291bde9
Md Zameer <165930608+mdzameercse@users.noreply.github.com>
2025-08-30 00:10:54 +0530

    Update 05_IsomorphicStrings.java

--- Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/05_IsomorphicStrings.java
+++ Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/05_IsomorphicStrings.java
@@ -2,7 +2,7 @@
 
 
 class Solution {
-    
+    //tc 0(N) and Sp 0(1)
     public boolean isIsomorphic(String s, String t) {
         
         if (s.length() != t.length()) {
@@ -27,3 +27,35 @@ class Solution {
         return true;
     }
 }
+//tc 0(N) and Sp 0(N)
+import java.util.*;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> mapped = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (map.containsKey(c1)) {
+                // already mapped, check consistency
+                if (map.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                // not mapped yet
+                if (mapped.contains(c2)) {
+                    return false; // another char already maps to c2
+                }
+                map.put(c1, c2);
+                mapped.add(c2);
+            }
+        }
+        return true;
+    }
+}
+
