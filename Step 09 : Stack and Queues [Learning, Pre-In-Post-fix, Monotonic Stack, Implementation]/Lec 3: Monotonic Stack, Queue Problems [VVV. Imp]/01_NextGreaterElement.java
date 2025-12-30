6e2945c988e5d67417bd8796ca7f656c648304a1
Md Zameer <165930608+mdzameercse@users.noreply.github.com>
2025-12-30 12:57:17 +0530

    Update 01_NextGreaterElement.java

--- Step 09 : Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3: Monotonic Stack, Queue Problems [VVV. Imp]/01_NextGreaterElement.java
+++ Step 09 : Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3: Monotonic Stack, Queue Problems [VVV. Imp]/01_NextGreaterElement.java
@@ -1,18 +1,19 @@
 class Solution {
     public ArrayList<Integer> nextLargerElement(int[] arr) {
     Stack<Integer> stack=new Stack<>();
-    ArrayList<Integer> list=new ArrayList<>();
+       ArrayList<Integer> list=new ArrayList<>();
     for(int i=arr.length-1;i>=0;i--)
     {
-            while(!stack.isEmpty() && stack.peek()<=arr[i])
-              stack.pop();// remove all which are less then current 
+            while(!stack.isEmpty() && stack.peek()<+arr[i])
+              stack.pop();// remove all which are more then current 
             
-            if (st.isEmpty()) res[i] = -1;
-            // Else top of stack is the answer
-            else res[i] = st.peek();
+            if (stack.isEmpty()) 
+                list.add(-1);
+            else 
+                list.add(stack.peek());
             stack.push(arr[i]);
     }
     Collections.reverse(list);
-     return list;   
+     return list;  
     }
 }
