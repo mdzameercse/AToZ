a74e79b925b554c4a490980e7a1ad8ff3fa5bb49
Md Zameer <165930608+mdzameercse@users.noreply.github.com>
2025-12-30 12:50:03 +0530

    Update 01_NextGreaterElement.java

--- Step 09 : Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3: Monotonic Stack, Queue Problems [VVV. Imp]/01_NextGreaterElement.java
+++ Step 09 : Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3: Monotonic Stack, Queue Problems [VVV. Imp]/01_NextGreaterElement.java
@@ -1,39 +1,18 @@
-// Question Link: https://leetcode.com/problems/next-greater-element-i/description/
-
-
 class Solution {
-
-    // Time Complexity = O(nums1.length + nums2.length), Space Complexity = O(nums2.length)
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        // Map to store next greater elements in nums2
-        HashMap<Integer, Integer> nextGreaterMap = new HashMap<>();
-        
-        // Stack to track decreasing elements
-        Stack<Integer> stack = new Stack<>();
-        
-        // Traverse nums2 to populate nextGreaterMap
-        for (int num : nums2) {
-            // Find next greater element for stack top
-            while (!stack.isEmpty() && stack.peek() < num) {
-                nextGreaterMap.put(stack.pop(), num);
-            }
-            stack.push(num); // Push current element
-        }
-        
-        // Remaining elements have no greater element
-        while (!stack.isEmpty()) {
-            nextGreaterMap.put(stack.pop(), -1);
-        }
-        
-        // Build result for nums1 using nextGreaterMap
-        int[] result = new int[nums1.length];
-        
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = nextGreaterMap.get(nums1[i]);
-        }
-
-       // return the result
-        return result;
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+    Stack<Integer> stack=new Stack<>();
+    ArrayList<Integer> list=new ArrayList<>();
+    for(int i=arr.length-1;i>=0;i--)
+    {
+            while(!stack.isEmpty() && stack.peek()<=arr[i])
+              stack.pop();// remove all which are less then current 
+            
+            if (st.isEmpty()) res[i] = -1;
+            // Else top of stack is the answer
+            else res[i] = st.peek();
+            stack.push(arr[i]);
+    }
+    Collections.reverse(list);
+     return list;   
     }
 }
